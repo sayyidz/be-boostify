b487bb3e846d1a8b61e2dd493857bddc3684c7ad
Mitchel <mitchel.mohamad@gmail.com>
2024-08-19 14:07:29 +0700

    adding some file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
 const Routes = require("./routes/routes");
+const attendanceRoutes = require('./routes/routes');
+const personalRecords = require('./routes/routes');
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.use("/api", Routes);
+
+// app.use(cors({
+// origin: [link]
+// }));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
